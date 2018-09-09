7bf1a0ab78ff7f0304200953c919237b776b3612
Koki Riho <riho.dare@gmail.com>
2018-09-09 16:22:37 +0900

    proposal

---
+++ StudySpport/StudySpport/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  StudySpport
+//
+//  Created by 利穂 虹希 on 2018/09/09.
+//  Copyright © 2018年 Koki Riho. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
